ae9326f82ea8290dc7b97ab4855fe9653510d7de
coutobeiso <matheuscouto0408@gmail.com>
2024-11-13 17:26:32 -0300

    beta

---
+++ chargle.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chargle</title>
+
+    <link rel="stylesheet" href="chargle.css">
+</head>
+
+<body>
+    <header>
+     <h1> <img src="220105.png" class="camaleao">CHARGLE </h1>
+     <nav>
+     <ul class="links">
+     <li><a href="">Lançamentos</a></li>
+     <li><a href="">Ofertas</a></li>
+     <li><a href="">Infantil</a></li>
+     <li><a href="">Feminino</a></li>
+     <li><a href="">Infantil</a></li>
+     <li><a href="">Sobre nós</a></li>
+     <li><a href="">Suporte</a></li>
+     </ul> 
+     </nav>
+        <div class="redes">
+            <a href="https://www.instagram.com/" target="_blank" ><img src="instagram.png" class="insta"></a>
+            <a href="https://www.facebook.com/?locale=pt_BR" target="_blank"><img src="facebook.png" alt="" class="face"></a>
+            <a href="https://x.com/?lang=pt-br" target="_blank"><img src="twitter.png" alt="" class="x"></a>
+        </div>
+
+        <div class="user">
+            <img src="usuario-de-perfil.png" alt="">
+        </div>
+
+
+    </header>
+    <main>
+        <section>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi mollitia veniam repellendus eveniet praesentium illo laboriosam, consectetur, laudantium molestias voluptatibus libero maiores provident magni reprehenderit ducimus temporibus officia, nesciunt consequuntur?
+        </section>
+        <section>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis dignissimos ex debitis eos libero nulla voluptate obcaecati, rem officia nemo nobis qui optio enim natus! Ut ipsam tempore beatae pariatur.
+        </section>
+    </main>
+
+</body>
+
+</html>
